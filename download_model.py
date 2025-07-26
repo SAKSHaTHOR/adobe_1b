444671027836fcd32718b71f681fded2782fa008
SAKSHaTHOR <130565461+SAKSHaTHOR@users.noreply.github.com>
2025-07-26 10:47:44 +0530

    Add files via upload
    first commit

---
+++ download_model.py
@@ -0,0 +1,6 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
+model.save('./local_model')
+
+print("local_model saved — ready for Docker!")
