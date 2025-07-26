2794f4c53fbc186a2d3d9e3526addb655854c799
Kajalyadav4 <140224853+Kajalyadav4@users.noreply.github.com>
2025-07-26 11:22:21 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-from sentence_transformers import SentenceTransformer
 
-# ✅ This ensures it downloads full model with pytorch_model.bin
-model = SentenceTransformer("sentence-transformers/paraphrase-MiniLM-L6-v2")
-
-# ✅ Save to ./local_model
-model.save("./local_model")
-
-print("✅ local_model downloaded and saved with pytorch_model.bin")
-=======
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
 model.save('./local_model')
 
 print("local_model saved — ready for Docker!")
->>>>>>> 444671027836fcd32718b71f681fded2782fa008
+
